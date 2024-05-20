b4d0bb9ad09927e81a87fb97ae122ca3f32accb2
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-05-20 10:46:18 +0000

    code change

--- 3rd_Algo/yoonpto/1316.py
+++ 3rd_Algo/yoonpto/1316.py
@@ -1,21 +1,25 @@
-a=int(input())
-string=[]
-count =0
-for i in range(a):
-  s= input()
-  string.append(s)
-for a in string:
-  last=a[0]  #마지막으로 나온 글자
-  dic={} #이미 나온 글자들
-  isbreak=False # 멈출지 말지
-  for c in a:
-    if(last!= c and dic.get(c,-1)!=-1): #딕셔너리 자료형(key value)
-      isbreak=True
-      break
-    dic[c]=dic.get(c,0)+1 # c=key 
-    last =c
-  if not isbreak:
-    count+=1
+N=int(input())
+count = 0
+for i in range(N):
+  word = input()
+  ## 이미 나왔던 것
+  exist = set()
+  last = word[0]
+  exist.add(last)
+  is_group_word = True
+  for i in range(1,len(word)):
+    ## 현재 문자
+    current = word[i]
+    ## 이미 존재하는 문자
+    if current in exist:
+        ## 저번 문자와 같지 않다면
+        if current != last:
+            is_group_word = False
+            break
+    else:
+        exist.add(current)
+    last = current
+  if is_group_word:
+        count += 1
 print(count)
-# get함수 (a,b) a= 찾고싶은 딕셔너리 key 값
-# b=인자가 없을 경우 출력하고 싶은 값
+
